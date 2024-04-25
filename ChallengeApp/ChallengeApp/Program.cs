ea2995f0e4c2289f7438ef367c905e252ba78375
Artur Borkowski <artbor2@gmail.com>
2024-04-25 12:04:09 +0200

    Arrays

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,37 +1,22 @@
-﻿/* Naapisz w Program.cs kod, w którym:
- * - zadeklarujesz zmienną z imieniem,
- * - zadeklarujesz zmienną z płcią (dobierz odpowiednia),
- * - zadeklarujesz zmienną z wiekiem
- * 
- * A następnie napisz kilka instrukcji warunkowych,
- * które przeanalizują te dane w następujący sposób:
- * 
- * (1) jeśli mamy do czynienia z kobietą i ma ponizej 30 lat,
- *     to zwróć komunikat: "Kobieta poniżej 30 lat"
- * (2)"jeśli mamy do czynienie z osobą o imieniu Ewa i ma ona równe 30 lat,
- *     to zwróć komunikat: "Ewa, lat 30"
- * (3) jeśli mamy do czynienia z mężczyzną i ma poniżej 18 lat zwróć komunikat:
- *     "Niepełnoletni mężczyzna"
- * (4) jeśli dane nie spełniają żadnego z warunków program nie zwraca nic
- * 
- * UWAGA
- * Twój program za wsze powinien zwracać TYLKO JRDEN KOMUNIKAT 
- * lub w przypadku (4) nic !
- */
+﻿int[] grades = new int[5];
+string[] dayOfWeeks = new string[7];
+dayOfWeeks[0] = "poniedziałek";
+dayOfWeeks[1] = "wtorek";
+dayOfWeeks[2] = "środa";
+dayOfWeeks[3] = "czwartek";
+dayOfWeeks[4] = "piatek";
+dayOfWeeks[5] = "sobota";
+dayOfWeeks[6] = "niedziela";
+Console.WriteLine(dayOfWeeks[2]);
+Console.WriteLine(dayOfWeeks.Length);
 
-var name = "Adam";
-var sex = 'M';
-var age = 20;
+string[] dayOfWeeks2 = new string[] {"poniedziałek", "wtorek", "środa",
+                                     "czwartek", "piątek", "sobota",
+                                     "niedziela"};
+Console.WriteLine(dayOfWeeks2[3]);
+Console.WriteLine(dayOfWeeks2.Length);
 
-if (sex == 'K' && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa, lat 30");
-}
-else if(sex == 'M' && age < 18)
-{
-    Console.WriteLine("Niepełnoletni meżczyzna");
-} 
+string[] dayOfWeeks3 = {"poniedziałek", "wtorek", "środa", "czwartek", 
+                        "piątek", "sobota", "niedziela"};
+Console.WriteLine(dayOfWeeks3[4]);
+Console.WriteLine(dayOfWeeks3.Length);
