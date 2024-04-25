272eabb34e02c84e4d4126a44a6800df66f0058e
Artur Borkowski <artbor2@gmail.com>
2024-04-25 17:53:09 +0200

    D5 - praca domowa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,33 @@
-﻿List<string> dayOfWeeks = new List<string>();
-dayOfWeeks.Add("poniedziałek");
-dayOfWeeks.Add("wtorek");
-dayOfWeeks.Add("środa");
-dayOfWeeks.Add("czwartek");
-dayOfWeeks.Add("piatek");
-dayOfWeeks.Add("sobota");
-dayOfWeeks.Add("niedziela");
+﻿/* Przygotuj program, który policzy ile jakich cyfr występuje
+ * w podanej liczbie.
+ *
+ * Przykład:
+ * Wyniki dla liczby 4566
+ * 0 => 0
+ * 1 => 0
+ * 2 => 0
+ * 3 => 0
+ * 4 => 1
+ * 5 => 1
+ * 6 => 2
+ * 7 => 0
+ * 8 => 0
+ * 9 => 0
+ */
 
-Console.WriteLine(dayOfWeeks.Count);
-Console.WriteLine();
-
-foreach (string day in dayOfWeeks)
+int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToCharArray();
+int[] digitsStats = new int[10];
+foreach (char c in letters)
 {
-    Console.WriteLine(day);
+    string digitAsString = c.ToString();
+    int digit = int.Parse(digitAsString);
+    digitsStats[digit]++;
+}
+
+Console.WriteLine(number);
+for (int i = 0; i < digitsStats.Length; i++)
+{
+    Console.WriteLine(i + " => " + digitsStats[i]);
 }
