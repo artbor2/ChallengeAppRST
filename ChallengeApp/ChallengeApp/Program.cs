5fd4f1cc83f7a89ec59fc708b75e2233f28af3ce
Artur Borkowski <artbor2@gmail.com>
2024-04-26 19:44:56 +0200

    zmienne imetody statyczne

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -3,14 +3,18 @@ User user2 = new User("Monika", "3636272727");
 User user3 = new User("Zuzia", "2666663436662");
 User user4 = new User("Damian", "2663748445r");
 
-var name = user1.Login;
 user1.AddScore(5);
 user1.AddScore(2);
 var result = user1.Result;
 Console.WriteLine(result);
+var name = User.GameName;
+var pi = Math.PI;
+User.Hello();
 
 internal class User
 {
+    public static string GameName = "Diablo";
+
     private List<int> scores = new List<int>();
 
     public User(string login, string password)
@@ -33,5 +37,10 @@ internal class User
     public void AddScore(int number)
     {
         this.scores.Add(number);
+    }
+
+    public static void Hello()
+    {
+        Console.WriteLine("Hello!");
     }
 }
