a024fd29fd47bf60a227d5692beff5826915287e
Artur Borkowski <artbor2@gmail.com>
2024-04-27 10:16:43 +0200

    D6 - praca domowa

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ChallengeApp
+{
+    internal class Employee
+    {
+        private List<int> scores = new List<int>();
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public int Result
+        {
+            get { return scores.Sum(); }
+        }
+
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public void AddScore(int score)
+        {
+            this.scores.Add(score);
+        }
+
+
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,12 +1,50 @@
-﻿using ChallengeApp;
+﻿/*
+ * 1. Stwórz klasę Employee, w której przechowasz imie, nazwisko, wiek
+ * oraz punktypracownika w postaci listy
+ * 2. Stwórz 3 pracowników i kazdemu przydziel po 5 ocen z zakresu od 1 do 10
+ * 3. Napisz program który wyszuka pracownika z najwyzszą liczbą ocen
+ * a następnie wyswietli jego dane oraz wynik.
+ */
 
-User user1 = new User("Adam", "3466534t7684");
-User user2 = new User("Monika", "3636272727");
-User user3 = new User("Zuzia", "2666663436662");
-User user4 = new User("Damian", "2663748445r");
+using ChallengeApp;
 
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
-var pi = Math.PI;
+Employee emp1 = new("Jan", "Abacki", 25);
+Employee emp2 = new("Tomasz", "Babacki", 35);
+Employee emp3 = new("Adam", "Cabacki", 31);
+
+emp1.AddScore(4);
+emp1.AddScore(7);
+emp1.AddScore(6);
+emp1.AddScore(5);
+emp1.AddScore(6);
+
+emp2.AddScore(5);
+emp2.AddScore(4);
+emp2.AddScore(7);
+emp2.AddScore(2);
+emp2.AddScore(9);
+
+emp3.AddScore(3);
+emp3.AddScore(4);
+emp3.AddScore(8);
+emp3.AddScore(9);
+emp3.AddScore(10);
+
+List<Employee> employees = new List<Employee>()
+{
+    emp1, emp2, emp3
+};
+
+Employee maxScoreEmployee = employees[0];
+foreach (var employee in employees)
+{
+    if (employee.Result > maxScoreEmployee.Result)
+    {
+        maxScoreEmployee = employee;
+    }
+}
+
+Console.WriteLine(maxScoreEmployee.Name);
+Console.WriteLine(maxScoreEmployee.Surname);
+Console.WriteLine(maxScoreEmployee.Age);
+Console.WriteLine("Scores: " + maxScoreEmployee.Result);
